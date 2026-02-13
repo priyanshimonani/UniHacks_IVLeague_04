7bc279e449404698995ef8464afcd7065b5586f1
priyanshimonani <priyanshimonani@gmail.com>
2026-02-14 02:28:57 +0530

    user join queue

--- UniHacks-Frontend/vite.config.js
+++ UniHacks-Frontend/vite.config.js
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),tailwindcss(),
+    }),
+    tailwindcss(),
   ],
+
+  // 🔥 ADD THIS SECTION
+  optimizeDeps: {
+    exclude: [
+      "framer-motion",
+      "canvas-confetti"
+    ]
+  }
 })
